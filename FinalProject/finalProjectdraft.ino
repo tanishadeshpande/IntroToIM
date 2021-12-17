3b00e13578768e5d95f6f4ce37ff6a3346ea9d36
tanishadeshpande <89835162+tanishadeshpande@users.noreply.github.com>
2021-12-17 22:45:27 +0400

    Update finalProjectdraft.ino

--- FinalProject/finalProjectdraft.ino
+++ FinalProject/finalProjectdraft.ino
@@ -11,7 +11,7 @@ const int keyC2 = 5;
 
 
 
-#include "pitches.h"
+
 
 
 void setup() {
@@ -27,20 +27,7 @@ void setup() {
   Serial.begin(9600);
 }
 
-//
-//void buttonPressed(key){
-//
-//  if (digitalRead(key) == HIGH){
-//
-//    return True;
-//  }
-//
-//  else {
-//
-//    return False
-//  }
-//
-//}
+
 
 
 
@@ -50,7 +37,7 @@ void loop() {
 
 if (digitalRead(keyC1) == HIGH){
 
-  //tone(spkrPin,NOTE_C4, 400);
+  
   Serial.print("C");
   delay(30);
  
@@ -58,7 +45,7 @@ if (digitalRead(keyC1) == HIGH){
 
 if (digitalRead(keyD) == HIGH){
 
-  //tone(spkrPin,NOTE_D4, 400);
+  
   Serial.print("D");
   delay(30);
  
@@ -66,7 +53,7 @@ if (digitalRead(keyD) == HIGH){
 
 if (digitalRead(keyE) == HIGH){
 
-  //tone(spkrPin,NOTE_E4, 400);
+  
   Serial.print("E");
   delay(30);
  
@@ -74,7 +61,7 @@ if (digitalRead(keyE) == HIGH){
 
 if (digitalRead(keyF) == HIGH){
 
-  //tone(spkrPin,NOTE_F4, 30);
+
   Serial.print("F");
   delay(30);
  
@@ -82,7 +69,6 @@ if (digitalRead(keyF) == HIGH){
 
 if (digitalRead(keyG) == HIGH){
 
-  //tone(spkrPin,NOTE_G4, 30);
   Serial.print("G");
   delay(30);
  
@@ -90,7 +76,6 @@ if (digitalRead(keyG) == HIGH){
 
 if (digitalRead(keyA) == HIGH){
 
-  //tone(spkrPin,NOTE_A4, 30);
   Serial.print("A");
   delay(30);
  
@@ -98,7 +83,6 @@ if (digitalRead(keyA) == HIGH){
 
 if (digitalRead(keyB) == HIGH){
 
-  //tone(spkrPin,NOTE_B4, 30);
   Serial.print("B");
   delay(30);
  
@@ -106,8 +90,9 @@ if (digitalRead(keyB) == HIGH){
 
 if (digitalRead(keyC2) == HIGH){
 
-  //tone(spkrPin,NOTE_C5, 30);
+
   Serial.print("c");
+  
   delay(30);
  
 } 
